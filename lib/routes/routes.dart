91c2c3616d403aa75a60d4c19098bcbef2d82924
clusterDe <dhannydhinata@gmail.com>
2025-07-05 20:02:32 +0700

    update routes

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:qansa/data/screens/home/home_screen.dart';
+import 'package:provider/provider.dart';
+
+import '../models/memes/components/memes.dart';
+import '../providers/memes_detail/memes_detail_provider.dart';
+import '../screens/home/home_screen.dart';
+import '../screens/memes_detail/memes_detail_screen.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
 
@@ -14,6 +19,14 @@ GoRouter route = GoRouter(
       path: HomeScreen.routeName,
       name: HomeScreen.routeName,
       builder: (context, state) => HomeScreen(),
+    ),
+    GoRoute(
+      path: MemesDetailScreen.routeName,
+      name: MemesDetailScreen.routeName,
+      builder: (context, state) => ChangeNotifierProvider.value(
+        value: MemesDetailProvider(data: state.extra as Memes),
+        child: MemesDetailScreen(),
+      ),
     ),
   ],
 );
